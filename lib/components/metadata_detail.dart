a793a043fe518f7892875eb430fca42f0c1595f1
fkn1017 <kosuke.fukano@gmail.com>
2020-05-31 20:31:12 +0900

    OGPが取得できなかったときの対処を追加。

--- lib/components/metadata_detail.dart
+++ lib/components/metadata_detail.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:metadata_fetch/metadata_fetch.dart';
 import 'package:provider/provider.dart';
 
+final noImagePath =
+    "https://www.shoshinsha-design.com/wp-content/uploads/2016/10/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88-2016-10-05-0.41.12.png";
+
 class MetadataDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,14 +15,14 @@ class MetadataDetail extends StatelessWidget {
         child: Column(
       children: <Widget>[
         Text(
-          _ogp.title,
+          _ogp.title ?? "No title",
           style: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 24,
           ),
         ),
-        Image.network(_ogp.image),
-        Text(_ogp.description)
+        Image.network(_ogp.image ?? noImagePath),
+        Text(_ogp.description ?? "No description")
       ],
     ));
   }
