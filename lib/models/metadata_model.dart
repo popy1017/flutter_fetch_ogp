b08600dea23a473484112f2466f21ffae53e3683
fkn1017 <kosuke.fukano@gmail.com>
2020-06-07 15:48:45 +0900

    共有されたデータを受信できるように修正

--- lib/models/metadata_model.dart
+++ lib/models/metadata_model.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:metadata_fetch/metadata_fetch.dart';
 import 'package:http/http.dart' as http;
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
 // テスト用。あとで消す。
 Map<String, dynamic> mockJson = {
@@ -12,6 +16,36 @@ Map<String, dynamic> mockJson = {
 
 class MetadataModel extends ChangeNotifier {
   Metadata ogp = Metadata.fromJson(mockJson);
+  StreamSubscription _intentDataStreamSubscription;
+
+  MetadataModel() {
+    // アプリ起動中の処理
+    _intentDataStreamSubscription =
+        ReceiveSharingIntent.getTextStream().listen((String value) {
+      final json = jsonDecode(value);
+      if (json["url"] != null) {
+        fetchOgpFrom(json["url"]);
+      }
+    }, onError: (err) {
+      print("getLinkStream error: $err");
+    });
+
+    // For sharing or opening urls/text coming from outside the app while the app is closed
+    ReceiveSharingIntent.getInitialText().then((String value) {
+      if (value != null) {
+        final json = jsonDecode(value);
+        if (json["url"] != null) {
+          fetchOgpFrom(json["url"]);
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _intentDataStreamSubscription.cancel();
+    super.dispose();
+  }
 
   Future<bool> fetchOgpFrom(String _url) async {
     try {
